beb2d57cfb5ecb04d80d920dbc1824ab46df452a
Riya-Kharade <riyasunilkharade.vit@gmail.com>
2025-03-04 23:52:58 +0530

    Add files via upload

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|RIYASUNIL\\riyas
+vti_modifiedby:SR|RIYASUNIL\\riyas
+vti_timelastmodified:TR|20 Jul 2023 04:12:21 -0000
+vti_timecreated:TR|20 Jul 2023 04:12:21 -0000
+vti_cacheddtm:TX|20 Jul 2023 04:12:21 -0000
+vti_filesize:IR|145007
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|my-friends.html guide.html enquiry.html contact-us.html my-family.html template.dwt skills.html qualification.html IT\\ 2023.html about-us.html index.html
